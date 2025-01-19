13498a3085013bb550f917058a7221eb15875ef6
Araucaria <tproctor@techvalleyhigh.org>
2025-01-19 16:59:48 -0500

    Added a constants file, worked on the ArmIO files

---
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems.arm;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public interface ArmIO {
+    // output
+    public void setArmVoltages(double pivotVoltage, double intakeVoltage);
+
+    // inputs
+    public boolean hasAlgae();
+
+    public Rotation2d currentAngle();
+
+    public double intakeSpeed(); 
+    //I would like to add a unit to this but I couldn't figureout what class to use 🤷‍♀️
+
+    default void stop() {
+        //Should this be in real? I don't think so as it's the same for both
+        setArmVoltages(0, 0);
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
@@ -0,0 +1,42 @@
+package frc.robot.subsystems.arm;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.AnalogInput;
+import frc.robot.Utils.Constants;
+
+public class ArmIOReal implements ArmIO{
+    SparkMax pivotMotor = new SparkMax(Constants.CANConstants.kPivotMotor, MotorType.kBrushless);
+    SparkMax intakeMotor = new SparkMax(Constants.CANConstants.kIntakeMotor, MotorType.kBrushless);
+    AnalogInput algaeSensor = new AnalogInput(Constants.AnalogInputConstants.kAlgaeSensor);
+    
+
+    @Override
+    public void setArmVoltages(double pivotVoltage, double intakeVoltage){
+        pivotMotor.setVoltage(pivotVoltage);
+        intakeMotor.setVoltage(intakeVoltage);
+    }
+
+    @Override
+    public boolean hasAlgae(){
+        //TODO: Make sure this value is right. I got this from 2024
+        return algaeSensor.getVoltage()>2.5;
+    }
+
+    @Override
+    public Rotation2d currentAngle(){
+        Rotation2d angle = Rotation2d.fromRotations(pivotMotor.getEncoder().getPosition());
+        return angle;
+    }
+
+    /*
+     * This is in RPM
+     */
+    @Override
+    public double intakeSpeed(){
+        double speed = intakeMotor.getEncoder().getVelocity();
+        return speed;
+    }
+}
