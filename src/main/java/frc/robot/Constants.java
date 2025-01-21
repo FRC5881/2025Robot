d43c8f68c47b1204a92954195611f8184f44c9c7
ricespider <gdandrade@techvalleyhigh.org>
2025-01-21 15:54:47 -0500

    Created a Climber subsystem with code for both a controlled climb with an e-stop and an uncontrolled climb. Created a Constants file, added climber constants, and a climber motor controller

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public class Constants {
+    public static class CANConstants {
+        // Climber
+        public static final int kClimberId = 1;
+    }
+
+    public static class ClimberConstants {
+        public static final double kClimberSpeed = 0.5;
+        public static final double kMinClimberSpeed = 0;
+        public static final double kMaxTorque = 10;
+    }
+}
