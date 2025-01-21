24bd670680d238aab52d9a4aa973e86856a8dd8a
ricespider <gdandrade@techvalleyhigh.org>
2025-01-21 16:04:34 -0500

    added the follower motor

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
     public static class CANConstants {
         // Climber
         public static final int kClimberId = 1;
+        public static final int kClimberFollowerId = 1;
     }
 
     public static class ClimberConstants {
