8b69f1665a8bd829964713055e6121dec3e042c1
Ryan Morash <ryan@morash.me>
2025-01-28 16:57:59 -0500

    Update build constants and improve Java VM settings for performance

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025Robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "3d9925108c59e2faec48c75c90dcdfc2ccbf2b65";
-  public static final String GIT_DATE = "2025-01-21 14:11:29 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-27 00:25:16 EST";
-  public static final long BUILD_UNIX_TIME = 1737955516410L;
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "b47497e19a597f275eabfb1899f4c8916b29cf21";
+  public static final String GIT_DATE = "2025-01-27 19:56:21 EST";
+  public static final String GIT_BRANCH = "RyanTest";
+  public static final String BUILD_DATE = "2025-01-28 15:27:00 EST";
+  public static final long BUILD_UNIX_TIME = 1738096020881L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
