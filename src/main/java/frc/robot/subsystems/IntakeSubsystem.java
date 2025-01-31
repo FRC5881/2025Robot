6aaca727c534f5a080c471c3fe0f57f00caba892
ricespider <gdandrade@techvalleyhigh.org>
2025-01-31 15:53:54 -0500

    Added Intake Subsystem

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+public class IntakeSubsystem {
+    public IntakeSubsystem() {
+        // Initialize motors, sensors, etc.
+    }
+
+    public void intake() {
+        // Intake coral
+    }
+
+    public void stop() {
+        // Stop intake
+    }
+}
