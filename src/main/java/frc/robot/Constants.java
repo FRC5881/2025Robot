59c9dd5cdc478e3e6e52149ddf4597bc37350cf6
ricespider <gdandrade@techvalleyhigh.org>
2025-02-17 14:18:17 -0500

    Added the intake subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,11 +5,17 @@ public class Constants {
         // Climber
         public static final int kClimberId = 1;
         public static final int kClimberFollowerId = 1;
+        // Intake
+        public static final int kIntakeId = 8;
     }
 
     public static class ClimberConstants {
         public static final double kClimberSpeed = 0.5;
         public static final double kMinClimberSpeed = 0;
         public static final double kMaxTorque = 10;
+    }
+
+    public static class IntakeConstants {
+        public static final double kIntakeSpeed = 1;
     }
 }
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,15 +1,31 @@
 package frc.robot.subsystems;
 
-public class IntakeSubsystem {
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class IntakeSubsystem extends SubsystemBase {
+    public final SparkMax intakeMotor;
+
     public IntakeSubsystem() {
-        // Initialize motors, sensors, etc.
+        intakeMotor = new SparkMax(Constants.CANConstants.kIntakeId, MotorType.kBrushless);
     }
 
     public void intake() {
         // Intake coral
+        intakeMotor.set(Constants.IntakeConstants.kIntakeSpeed);
     }
 
     public void stop() {
         // Stop intake
+        intakeMotor.stopMotor();
     }
+
+    public void reverse() {
+        // Reverse intake
+        intakeMotor.set(-Constants.IntakeConstants.kIntakeSpeed);
+    }
+
 }
