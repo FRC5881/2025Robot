c46b4f36f66779aa345b04b0738d69e331339c76
ricespider <gdandrade@techvalleyhigh.org>
2025-02-17 15:30:17 -0500

    added an L2 subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,11 +5,18 @@ public class Constants {
         // Climber
         public static final int kClimberId = 1;
         public static final int kClimberFollowerId = 1;
+
+        // L2
+        public static final int kL2Id = 1;
     }
 
     public static class ClimberConstants {
         public static final double kClimberSpeed = 0.5;
         public static final double kMinClimberSpeed = 0;
         public static final double kMaxTorque = 10;
+    }
+
+    public static class L2Constants {
+        public static final double kL2Speed = 1;
     }
 }
---
+++ src/main/java/frc/robot/subsystems/L2Subsystem.java
@@ -0,0 +1,53 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+public class L2Subsystem extends SubsystemBase {
+    public final SparkMax L2Motor;
+    public final boolean Sensor1;
+    public final boolean Sensor2;
+
+    public L2Subsystem() {
+        L2Motor = new SparkMax(Constants.CANConstants.kL2Id, MotorType.kBrushless);
+        Sensor1 = false;
+        Sensor2 = false;
+
+    }
+
+    public void output(double speed) {
+        L2Motor.set(speed);
+    }
+
+    public void center(double speed) {
+        if (Sensor1) {
+            L2Motor.set(speed);
+        } else if (Sensor2) {
+            L2Motor.set(-speed);
+        } else
+            L2Motor.set(0);
+    }
+
+    public void stop() {
+        L2Motor.set(0);
+    }
+
+    public Command cOutputLeft() {
+        double speed = Constants.L2Constants.kL2Speed;
+        return startEnd(() -> output(speed), () -> stop());
+    }
+
+    public Command cCenter() {
+        double speed = Constants.L2Constants.kL2Speed;
+        return startEnd(() -> center(speed), () -> stop());
+    }
+
+    public Command cOutputRight() {
+        double speed = -Constants.L2Constants.kL2Speed;
+        return startEnd(() -> output(speed), () -> stop());
+    }
+}
